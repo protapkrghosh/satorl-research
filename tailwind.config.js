b2af75b9d40216551eaba00414949b94cc20ac1a
Protap Ghosh <protapg518@gmail.com>
2024-02-02 01:07:45 +0600

    added favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         fontFamily: {
+            dmSans: "DM Sans",
+            dmSerifDisplay: "DM Serif Display",
+            inter: "Inter",
+         },
+      },
    },
    plugins: [require("daisyui")],
 };
