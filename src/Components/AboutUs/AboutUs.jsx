a9d90cfb3500f4adcf2e36f875271b720413631f
Protap Ghosh <protapg518@gmail.com>
2024-02-02 09:40:27 +0600

    Added about us section

---
+++ src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,41 @@
+import Container from "../Container/Container";
+import aboutImg from '../../assets/about us/Looper1.png'
+import circleImg from '../../assets/about us/design 2.png'
+
+const AboutUs = () => {
+  return (
+    <Container>
+      <div className="md:flex justify-between items-center">
+        <div className="md:w-1/2">
+          <img src={aboutImg} alt="Image" className="" />
+        </div>
+
+        <div className="md:w-1/2 md:-mr-8 xl:-mr-20 mt-20 md:mt-0">
+          <div>
+            <div className="relative">
+              <div className="absolute bottom-[108px] md:bottom-[100px] -left-[10px] md:-left-[44px] w-full">
+                <img src={circleImg} alt="Image" className="w-[60px] md:w-[90px]" />
+                <p className="text-[#F6524E] text-[14px] md:text-[17px] font-semibold font-inter uppercase absolute top-7 left-8  md:top-14 md:left-12">About Us</p>
+              </div>
+              <h1 className="text-[38px] md:text-[35px] xl:text-[44px] text-[#441611] font-dmSerifDisplay capitalize leading-[62px] mb-[16px]">Innovating Visionary <br /> Solutions Together</h1>
+            </div>
+
+            <div className="font-dmSans capitalize">
+              <p className="text-[20px] font-semibold mb-[30px] w-full md:w-[95%]">
+                <span className="text-[#7c5c58]">At Satori Ventures,</span>
+                <span className="text-[#817d7d]"> we recognize the transformative power of blockchain</span>
+              </p>
+
+              <div className="text-[#6F5D5B] md:w-[95%] xl:w-[83%] space-y-5">
+                <p>We invest in the potential of tomorrow, employing a keen, eagle-eyed approach to pinpoint opportunities that redefine what's possible.</p>
+                <p>Our global perspective, fortified by a robust Asian market presence, ensures that our partners are poised for international influence and success.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default AboutUs;
