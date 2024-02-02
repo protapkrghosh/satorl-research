ef5eb08827cd85db94b571faba09087a4b020316
Protap Ghosh <protapg518@gmail.com>
2024-02-02 16:32:50 +0600

    added footer and not found section

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../assets/footer/Group 71033.png'
+import Container from '../../Components/Container/Container';
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is footer section</h2>
+    <div className='bg-[#441611] pt-12'>
+      <Container>
+        <div className='md:flex justify-between items-center mb-12'>
+          <Link to="/">
+            <img src={img} alt="Image" className='w-[130px]' />
+          </Link>
+
+          <div className='font-dmSans flex gap-x-10 text-white'>
+            <Link to="/">Home</Link>
+            <Link to="/">Portfolio</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+          </div>
+        </div>
+      </Container>
+      <p className='text-white text-[16px] text-center font-dmSans uppercase py-5 border-t border-[#4D211D]'>©{new Date().getFullYear()} SATORI RESEARCH. All Rights Reserved.</p>
     </div>
   );
 };
