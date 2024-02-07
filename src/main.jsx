3b39ef5ca71d37d3130bdf9e2501d1f5063f6c18
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-02-07 20:37:15 +0600

    corsor animation

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { router } from './Routes/Routes'
 import { RouterProvider } from 'react-router-dom'
+import AnimCursor from './Components/AnimCursor/AnimCursor'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AnimCursor />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
