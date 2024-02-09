33a9c8d1bfd8298bcd4e6476e697d07e5c995283
Protap Ghosh <protapg518@gmail.com>
2024-02-09 14:31:47 +0600

    added globe

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,8 +2,11 @@ import Container from "../Container/Container";
 import icon from '../../assets/header people/Vector.png'
 import bannerImg from '../../assets/VCS landing page/Group 71031@2x.png'
 import avatar from '../../assets/header people/Group 71017.png'
+import GlobeComponent from "../GlobeComponent/GlobeComponent";
+
 
 const Banner = () => {
+  
   return (
     <Container>
       <div className="flex flex-col md:flex-row justify-center md:justify-between items-center mt-16 mb-[85px]" id="home">
@@ -22,10 +25,15 @@ const Banner = () => {
           <p className="text-[#5D423F] font-dmSans capitalize w-[86%] 2xl:w-[54%] mt-1" data-aos="fade-up" data-aos-duration="1500">the transformative power of blockchain and its capacity to redefine economies. We invest in the potential.</p>
         </div>
 
-        {/* Image */}
-        <div className="md:w-1/2 mt-14 md:mt-0" data-aos="zoom-in" data-aos-duration="1000">
-          <img src={bannerImg} alt="Image" className="w-[80%] xl:w-[80%] md:w-full mx-auto heroImg" />
+        {/* Globe */}
+        <div className="md:w-1/2">
+          <div>
+            <GlobeComponent />
+          </div>
         </div>
+        
+
+        
       </div>
     </Container>
   );
